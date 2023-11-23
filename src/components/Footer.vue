3987551b2cba4db004b1b99de6495555077fa95b
rpsobolewski <rpsobolewski@gmail.com>
2023-11-23 15:49:22 +0100

    navbar + footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,52 @@
+<script >
+
+export default {
+    name: 'Footer',
+
+
+
+}
+
+</script>
+
+<template>
+    <div class="footer">
+        <div class="container">
+            <h5 class="mb-3">DEVS</h5>
+            <div class="row justify-content-center">
+                <div class="col-md-2">
+                    <a href="https://github.com/vgianluca96" target="_blank">vgianluca96</a>
+                </div>
+                <div class="col-md-2">
+                    <a href="https://github.com/LiciaLicari" target="_blank">LiciaLicari</a>
+                </div>
+                <div class="col-md-2">
+                    <a href="https://github.com/FedericaCampo97" target="_blank">FedericaCampo97</a>
+                </div>
+                <div class="col-md-2">
+                    <a href="https://github.com/rpsobolewski" target="_blank">rpsobolewski</a>
+                </div>
+                <div class="col-md-2">
+                    <a href="https://github.com/RiccardoSansone" target="_blank">RiccardoSansone</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+    background-color: #430030;
+    color: #ffecde;
+    text-align: center;
+    padding: 20px;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+
+.footer a {
+    color: #ffecde;
+    text-decoration: none;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<script >
+
+export default {
+    name: 'Navbar',
+
+
+
+}
+
+</script>
+
+<template>
+    <nav id="navbar" class="nav justify-content-center   w-100 shadow">
+
+        <router-link class="nav-link" to="/">Home</router-link>
+        <router-link class="nav-link" to="/about">About Us</router-link>
+        <router-link class="nav-link" to="/contacts">Contact Us</router-link>
+
+
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+.nav-link {
+    color: #ffecde;
+}
+
+nav {
+    background-color: #430030;
+}
+</style>
