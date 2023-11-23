04673c688c714ca85233baac6512cb5b1842d142
FedericaCampo97 <federica.campo97@gmail.com>
2023-11-23 21:54:43 +0100

    Add card cocktails and show single cocktail

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from './views/HomeView.vue';
+import ShowCocktail from './views/ShowCocktail.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -9,6 +10,11 @@ const router = createRouter({
             path: '/',
             name: 'home',
             component: HomeView
+        },
+        {
+            path: `/show/:id`,
+            name: 'show',
+            component: ShowCocktail
         },
     ]
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,46 @@
 <script>
-
+import axios from 'axios';
 export default {
-    name: 'HomeView'
+    name: 'HomeView',
+    data() {
+        return {
+            fullUrl: 'http://127.0.0.1:8001/api/cocktails',
+            cocktails: []
+        }
+    },
+    methods: {
+        async getCocktails() {
+            const response = await axios.get(this.fullUrl).then(resp => {
+                return resp
+            })
+            this.cocktails = response.data.result
+            console.log(this.cocktails);
+        }
+    },
+    mounted() {
+        this.getCocktails()
+    }
 }
 
 </script>
 
 <template>
-    <div class="img_home text-white ">
-        <h1>COCKTAIL DA SOGNO A PORTATA DI UN CLICK</h1>
+    <div class="img_home text-white d-flex align-items-end justify-content-center">
+        <h1 class="mb-5">DREAM COCKTAILS JUST A CLICK AWAY</h1>
     </div>
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                            card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div style="background-color: #1e0904;">
+        <div class="container pt-5">
+            <h2 class="mb-5 text-center text-white">OUR PROPOSAL</h2>
+            <div class="row">
+                <div class="col" v-for="cocktail in  cocktails ">
+                    <div class="card mb-5 demo" style="width: 18rem;">
+                        <img :src="cocktail.strDrinkThumb" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ cocktail.strDrink }}</h5>
+                            <p class="card-text mb-0"><strong>Category:</strong> {{ cocktail.strCategory }}</p>
+                            <p class="card-text"><strong>Glass:</strong> {{ cocktail.strGlass }}</p>
+                            <a :href="'/show/' + cocktail.id" class="btn btn_card mt-3"><strong>Go to cocktail</strong></a>
+                        </div>
                     </div>
                 </div>
             </div>
