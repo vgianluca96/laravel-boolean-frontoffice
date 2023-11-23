32077784742762ec4245c56d1d360c4be50d9ee8
FedericaCampo97 <federica.campo97@gmail.com>
2023-11-23 20:07:47 +0100

    modifiche

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,8 +7,23 @@ export default {
 </script>
 
 <template>
-    <div class="container py-4">
-        Hello
+    <div class="img_home text-white ">
+        <h1>COCKTAIL DA SOGNO A PORTATA DI UN CLICK</h1>
+    </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
