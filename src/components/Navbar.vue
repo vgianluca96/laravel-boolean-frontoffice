def32b0139dcb1ce27ec0f3fd25844c628b2aa11
rpsobolewski <rpsobolewski@gmail.com>
2023-11-24 10:01:35 +0100

    style change

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,22 +10,89 @@ export default {
 </script>
 
 <template>
-    <nav id="navbar" class="nav justify-content-center   w-100 shadow">
-
-        <router-link class="nav-link" to="/">Home</router-link>
-        <router-link class="nav-link" to="/about">About Us</router-link>
-        <router-link class="nav-link" to="/contacts">Contact Us</router-link>
+    <ul id="navbar" class="nav justify-content-center   w-100 shadow">
 
 
-    </nav>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About Us</router-link></li>
+        <li><router-link to="/contacts">Contact Us</router-link></li>
+
+
+    </ul>
 </template>
 
 <style lang="scss" scoped>
+svg {
+    fill: #ffecde
+}
+
 .nav-link {
     color: #ffecde;
 }
 
-nav {
+.nav {
     background-color: #430030;
+    padding-top: 25px;
+    padding-bottom: 5px;
+}
+
+
+
+ul {
+    margin: 0;
+    padding: 0;
+    display: flex;
+}
+
+ul li {
+    list-style: none;
+    margin: 0 20px;
+    transition: 0.5s;
+}
+
+ul li a {
+    display: block;
+    position: relative;
+    text-decoration: none;
+    padding: 5px;
+    font-size: 18px;
+    font-family: sans-serif;
+    color: #ffecde;
+    text-transform: uppercase;
+    transition: 0.5s;
+}
+
+ul:hover li a {
+    transform: scale(1.5);
+    opacity: 0.2;
+    filter: blur(5px);
+}
+
+ul li a:hover {
+    transform: scale(2);
+    opacity: 1;
+    filter: blur(0);
+    text-decoration: none;
+    color: #430030;
+}
+
+ul li a:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #ffecde;
+    transition: 0.5s;
+    transform-origin: right;
+    transform: scaleX(0);
+    z-index: -1;
+}
+
+ul li a:hover:before {
+    transition: transform 0.5s;
+    transform-origin: left;
+    transform: scaleX(1);
 }
 </style>
