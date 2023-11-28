d7385935cf7c543691a6c6ffd8c4cbc3f677b93d
rpsobolewski <rpsobolewski@gmail.com>
2023-11-28 15:19:13 +0100

    title , about style change

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -8,27 +8,34 @@ export default {
 
 <template>
     <div style="background-color: #1e0904;">
-        <div class="container py-5">
+        <div class="container main py-5">
             <div style="background-color: rgb(110, 73, 67);" class="p-5 mb-4 rounded-3 d-flex">
                 <div class="container-fluid py-5 text-center text-white">
-                    <h1  style="color: darkorange;" class="display-5 fw-bold">About</h1>
+                    <h1 style="color: darkorange;" class="display-5 fw-bold">About</h1>
                     <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate error sit esse
-                        libero labore voluptatem, architecto veritatis porro sed consequatur incidunt aliquid soluta inventore
+                        libero labore voluptatem, architecto veritatis porro sed consequatur incidunt aliquid soluta
+                        inventore
                         aut a nostrum alias non reiciendis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-                        quibusdam tempore nemo saepe commodi eligendi, iusto amet at fugiat quos repudiandae, beatae delectus
+                        quibusdam tempore nemo saepe commodi eligendi, iusto amet at fugiat quos repudiandae, beatae
+                        delectus
                         fugit voluptates! Blanditiis beatae suscipit dolores tenetur?
                         Molestiae totam necessitatibus iste blanditiis repudiandae ex quidem accusantium laborum tempore
-                        reprehenderit at nostrum voluptas dicta autem adipisci perspiciatis excepturi numquam distinctio quos
+                        reprehenderit at nostrum voluptas dicta autem adipisci perspiciatis excepturi numquam distinctio
+                        quos
                         consequuntur sapiente repellendus, optio impedit porro! Alias.
                         Incidunt voluptate nesciunt doloribus est earum possimus, expedita maiores, maxime numquam alias
                         molestiae reprehenderit quas soluta impedit exercitationem harum. Iusto obcaecati illum error eaque
                         dicta deserunt nostrum iste maxime numquam!</p>
                     <RouterLink to="/" class="btn btn_card btn-lg" type="button">Home</RouterLink>
+                </div>
+
             </div>
-    
-        </div>
         </div>
     </div>
 </template>
 
-<style></style>
+<style>
+.main {
+    height: 85vh;
+}
+</style>
